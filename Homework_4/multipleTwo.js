aee079fe3328d35abd9cd64d3324c5d93c4be4f7
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-05-03 13:15:47 +0400

    Change commit

--- Homework_4/multipleTwo.js
+++ Homework_4/multipleTwo.js
@@ -2,12 +2,12 @@ let menu = {
   width: 200,
   height: 300,
   title: "My menu",
+};
+
+for (let key in menu) {
+  if (typeof menu[key] === "number") {
+    menu[key] = menu[key] * 2;
+  }
 }
 
-for(let key in menu){
-    if(typeof menu[key] === "number"){
-        menu[key] = menu[key] * 2
-    }
-}
-
-console.log(menu)
+console.log(menu);

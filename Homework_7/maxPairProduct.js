8f56ef6f09d81e4415d0001d1fb94e507d6bbce2
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-05-06 17:27:14 +0400

    feat:found the maximum value of the product of adjacent array elements

---
+++ Homework_7/maxPairProduct.js
@@ -0,0 +1,11 @@
+function getMaxPairProduct(arr) {
+    let maxProduct = arr[0] * arr[1]
+    for(let i = 1; i < arr.length - 1; i++){
+        if(arr[i] * arr[i + 1] > maxProduct){
+            maxProduct = arr[i] * arr[i + 1]
+        }
+    }
+    return maxProduct
+}
+
+console.log(getMaxPairProduct([1, 2, 3, 5, 0, 6, 73, 5, 31, 511, 500]));

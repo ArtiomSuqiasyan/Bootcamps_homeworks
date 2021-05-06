58bc2354be4a36f31e2ad4d4d2dec82a0dacb50b
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-05-06 17:45:37 +0400

    feat:function takes in a string and returns a function that returns string

--- Homework_7/redundantFunc.js
+++ Homework_7/redundantFunc.js
@@ -2,7 +2,7 @@ function redundant (str = "") {
     return () => str
 }
 
-const f1 = redundant("apple");
+const f1 = redundant("apples");
 console.log(f1())
 const f2 = redundant("pear");
 console.log(f2())

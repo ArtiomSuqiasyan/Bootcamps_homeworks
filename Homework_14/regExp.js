499a3c73c3ff438a668f58da3961dc531421f1ef
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-05-21 14:45:38 +0400

    Feat: Add regExp exercises

---
+++ Homework_14/regExp.js
@@ -0,0 +1,6 @@
+// function checkRegExp(str) {
+//   const checkNumAndLength = /^(\d{4}|\d{6})$/;
+//   const checkSpace = /^\S*$/;
+//   return checkSpace.test(str) && checkNumAndLength.test(str);
+// }
+// console.log(checkRegExp("127411"));

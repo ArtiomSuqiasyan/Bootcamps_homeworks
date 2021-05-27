de53451eef9cba0214054a9fb930a3aea6e505ca
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-05-27 11:57:39 +0400

    Fix: Fixed default argument for constructor

--- Homework_17/customConsole.js
+++ Homework_17/customConsole.js
@@ -1,28 +1,27 @@
 class CustomConsole {
-    constructor(arg = "logs"){
-        this.arg = arg
-        this.logs = []
-    }
+  constructor(arg = "") {
+    this.arg = arg;
+    this.logs = [];
+  }
 
-    log(...arg){
-        
-        let strArg = [];
-        arg.forEach((el) => strArg.push(JSON.stringify(el)))
-        strArg = strArg.join(", ")
-        this.logs.push(strArg)
-        return `"${this.arg}: ${strArg}"`
-    }
+  log(...arg) {
+    let strArg = [];
+    arg.forEach((el) => strArg.push(JSON.stringify(el)));
+    strArg = strArg.join(", ");
+    this.logs.push(strArg);
+    return `"${this.arg}: ${strArg}"`;
+  }
 
-    history(start = 0, end = this.logs.length){
-        return this.logs.slice(start, end)
-    }
+  history(start = 0, end = this.logs.length) {
+    return this.logs.slice(start, end);
+  }
 
-    clearHistory(){
-        this.logs = [];
-        return "''"
-    }
+  clearHistory() {
+    this.logs = [];
+    return "''";
+  }
 }
 
 let myConsole = new CustomConsole("Regular");
-console.log(myConsole.log(1, 2, 3, [1, 2]))
-console.log(myConsole.clearHistory())
+console.log(myConsole.log(1, 2, 3, [1, 2]));
+console.log(myConsole.clearHistory());
